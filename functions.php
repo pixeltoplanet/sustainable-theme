8ad0ce6a1e2d7d28ee825f636bf87270949a47a9
dannymoons <danny@hulpmetmarketing.nl>
2025-11-21 09:48:11 +0100

    chore: bump version to 0.1.0
    - Update version in package.json to 0.1.0
    - Update SUSTAINABLE_THEME_VERSION constant to 0.1.0
    - Reset versioning for initial release

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define theme constants
-define('SUSTAINABLE_THEME_VERSION', '1.0.0');
+define('SUSTAINABLE_THEME_VERSION', '0.1.0');
 define('SUSTAINABLE_THEME_DIR', get_template_directory());
 define('SUSTAINABLE_THEME_URL', get_template_directory_uri());
 
